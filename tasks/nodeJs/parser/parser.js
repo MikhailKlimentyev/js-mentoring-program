b046e01ecbc691f45f2a1406a96ad0ca6097b71d
Mikhail_Klimentsyeu <mikhail_klimentsyeu@epam.com>
2021-12-13 22:10:13 +0300

    Module 8. Parser

--- tasks/nodeJs/parser/parser.js
+++ tasks/nodeJs/parser/parser.js
@@ -24,10 +24,25 @@
 const fs = require('fs');
 
 const jsonParser = () => {
-
+	fs.readFile('./tasks/nodeJs/parser/test.json', 'utf-8', (err, content) => {
+		if (err) {
+			console.log(err);
+		}
+		const parsed = JSON.parse(content)
+			.list.entries
+			.map((entries) => "http://doc.epam.com/" + entries.entry.name.slice(0, -5))
+			.map((docId) => ({
+				'docId': docId
+			}));
+		fs.writeFile('./tasks/nodeJs/parser/parsed.json', JSON.stringify(parsed), err => {
+			if (err) {
+				console.log(err);
+			}
+			console.log('the file is created')
+		})
+	})
 };
 
 module.exports = {
 	jsonParser
 };
-
